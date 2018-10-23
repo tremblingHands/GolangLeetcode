abf44d9c2474fb4d6f750866b9bc08c3823aeef0
tremblingHands <605225331@qq.com>
2018-10-23 19:03:27 +0800

    add 146

---
+++ 146/146.go
@@ -0,0 +1,84 @@
+type node struct{
+	k,v int
+	pre, next *node
+}
+
+type Chain struct{
+	head *node
+	tail *node
+}
+
+type LRUCache struct {
+    capacity int
+	length int
+	table map[int]*node
+	chain Chain
+}
+
+
+func Constructor(capacity int) LRUCache {
+	tmp := &node{}
+	return LRUCache{
+		capacity,
+		0,
+		make(map[int]*node),
+		Chain{
+			head: tmp,
+			tail: tmp,
+		},
+	}
+}
+
+
+func (this *LRUCache) Get(key int) int {
+		if n, o := this.table[key];!o{
+			return -1
+		}else{
+			if n != this.chain.tail{
+				n.next.pre = n.pre
+				n.pre.next = n.next
+				this.chain.tail.next = n
+				n.pre = this.chain.tail
+				n.next = nil
+				this.chain.tail = n
+			}
+			return n.v
+		}
+}
+
+
+func (this *LRUCache) Put(key int, value int)  {
+		if n, o := this.table[key];o{
+			n.v = value
+			this.Get(key)
+			return
+		}
+		in := &node{
+			v:value,
+			k:key,
+		}
+    	if this.length<this.capacity{
+			in.pre = this.chain.tail
+			this.chain.tail.next = in
+			this.chain.tail = in
+			this.table[key] = in
+			this.length++
+		}else{
+			out := this.chain.head.next
+            delete(this.table, out.k)
+            this.chain.head.next = out.next
+            if out.next != nil{ 
+                out.next.pre =  this.chain.head
+            }
+            if out.next == nil{ 
+                this.chain.tail = this.chain.head
+            }
+            in.pre = this.chain.tail
+            this.chain.tail.next = in
+            this.chain.tail = in
+            this.table[key] = in
+		}
+
+}
+
+
